1a367a005434d17a14a8dd2644ffa908202f7ca2
Conor Wantuch <cwantuch7@gmail.com>
2025-04-07 16:38:47 -0600

    finished most of it

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,25 @@
+
+
+const BurgerStack = (props) => {
+    return (
+        <ul>
+        {props.stack.length === 0 ? (
+          <li>No ingredients added yet!</li>
+        ) : (
+          props.stack.map((ingredient, index) => (
+            <li
+              key={index} style={{ backgroundColor: ingredient.color }}>
+              {ingredient.name}
+              <button
+                onClick={() => props.removeFromBurger(index)}>
+                X
+              </button>
+            </li>
+          ))
+        )}
+      </ul>
+    )
+  };
+  
+  export default BurgerStack;
+  
